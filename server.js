4bed5dbdd35c61dec5e55c4ed4b8a1c52d248803
aa1aac <ach.aayush.111@gmail.com>
2019-09-29 18:28:22 +0545

    connect to mongodb server

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
 const express = require("express");
 
+const connectDB = require("./config/db.js");
+
 const app = express();
+
+// connect database
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 
-// 6.4
+// 6.5
 
 app.get("/", (req, res) => {
   res.json("hello world");
 });
 
-app.use('/api/users',require('./routes/users'))
-app.use('/api/contacts',require('./routes/contacts'))
-app.use('/api/auth',require('./routes/auth'))
+app.use("/api/users", require("./routes/users"));
+app.use("/api/contacts", require("./routes/contacts"));
+app.use("/api/auth", require("./routes/auth"));
 
 app.listen(PORT, () => {
   console.log(`backend server running on port ${PORT}`);
