56230c74cbd49af60d5900e441ec009c86caf303
aa1aac <ach.aayush.111@gmail.com>
2019-10-02 19:01:28 +0545

    user authorization backend

--- routes/auth.js
+++ routes/auth.js
@@ -5,10 +5,18 @@ const { check, validationResult } = require("express-validator");
 const config = require("config");
 
 const User = require("../models/User");
+const auth = require("../middleware/auth");
+
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("get logged in user");
+router.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    return res.json(user);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send("server error");
+  }
 });
 
 router.post(
@@ -27,7 +35,7 @@ router.post(
     const { email, password } = req.body;
 
     try {
-      let user = User.findOne({ email });
+      let user = await User.findOne({ email });
 
       if (!user) {
         return res.status(400).json({ msg: "Invalid credentials" });
@@ -49,16 +57,16 @@ router.post(
         payload,
         config.get("jwtSecret"),
         {
-          expiresIn: 360000
+          expiresIn: 3600000
         },
         (err, token) => {
           if (err) throw err;
-          res.json({ token });
+          return res.json({ token });
         }
       );
     } catch (err) {
-      console.error(err.message);
-      res.status(500).send("server error");
+      console.log(err);
+      return res.status(500).send("server error");
     }
   }
 );
