124851584e4fd6a9948d4c382807aa0f2c4d862c
aa1aac <ach.aayush.111@gmail.com>
2019-10-20 16:51:27 +0545

    add contact and private routing

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ import AlertState from "./context/alert/AlertState";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import setAuthToken from "./utils/setAuthToken";
+import PrivateRoute from "./components/routing/PrivateRoute";
 
 import "./App.css";
 
@@ -29,7 +30,7 @@ function App() {
               <div className="container">
                 <Alerts />
                 <Switch>
-                  <Route exact path="/" component={Home} />
+                  <PrivateRoute exact path="/" component={Home} />
                   <Route exact path="/about" component={About} />
                   <Route exact path="/register" component={Register} />
                   <Route exact path="/login" component={Login} />
--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -1,5 +1,5 @@
 import React, { useReducer } from "react";
-import uuid from "uuid";
+import axios from "axios";
 
 import ContactContext from "./ContactContext";
 import ContactReducer from "./ContactReducer";
@@ -10,43 +10,35 @@ import {
   CLEAR_CURRENT,
   UPDATE_CONTACT,
   FILTER_CONTACTS,
-  CLEAR_FILTER
+  CLEAR_FILTER,
+  CONTACT_ERROR
 } from "../types";
 
 const ContactState = props => {
   const initialState = {
-    contacts: [
-      {
-        type: "professional",
-        date: "2019-10-03T12:16:09.212Z",
-        id: "5d95e78736461e26acc04106",
-        name: "Mr Smith",
-        email: "smith@smith.com",
-        phone: "000-000-000-001",
-        user: "5d934ce59e3db603685f21df",
-        __v: 0
-      },
-      {
-        type: "personal",
-        date: "2019-10-03T12:16:09.212Z",
-        id: "5d95e81c36461e26acc04108",
-        name: "harry whity",
-        email: "harry@gmail.com",
-        phone: "111-111-111",
-        user: "5d934ce59e3db603685f21df",
-        __v: 0
-      }
-    ],
+    contacts: [],
     current: null,
-    filtered: null
+    filtered: null,
+    error: null
   };
 
   const [state, dispatch] = useReducer(ContactReducer, initialState);
 
   // add contact
-  const addContact = contact => {
-    contact.id = uuid.v4();
-    dispatch({ type: ADD_CONTACT, payload: contact });
+  const addContact = async contact => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    };
+
+    try {
+      const res = await axios.post("/api/contacts", contact, config);
+
+      dispatch({ type: ADD_CONTACT, payload: res.data });
+    } catch (err) {
+      dispatch({ type: CONTACT_ERROR, payload: err.response.msg });
+    }
   };
   // delete contact
   const deleteContact = id => {
@@ -83,6 +75,7 @@ const ContactState = props => {
         contacts: state.contacts,
         current: state.current,
         filtered: state.filtered,
+        error: state.error,
         addContact,
         deleteContact,
         setCurrent,
